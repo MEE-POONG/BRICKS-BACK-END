3992f472b8ebd8f850245feb16d9a8fed5f4f92b
Primpunyaporn <117614438+Primpunyaporn@users.noreply.github.com>
2023-02-04 11:47:41 +0700

    eiei

---
+++ pages/team/teamType.jsx
@@ -0,0 +1,101 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from 'react'
+import IndexPage from "components/layouts/IndexPage"
+import { Container, Modal, Card, Button, Form, Image, InputGroup, Row, Col, Table, Pagination, Badge } from 'react-bootstrap'
+import MyPagination from "@/components/Pagination"
+import useAxios from 'axios-hooks'
+import PageLoading from '@/components/PageChange/pageLoading'
+import PageError from '@/components/PageChange/pageError'
+import teamTypeEditModal from '@/container/teamType/teamTypeEditModal'
+import teamTypeDeleteModal from '@/container/teamType/teamTypeDeleteModal'
+import teamTypeAddModal from '@/container/teamType/teamTypeAddModal'
+
+export default function teamTypePage() {
+    const [params, setParams] = useState({
+        page: '1',
+        pageSize: '10'
+    });
+
+    const [{ data: teamTypeData, loading, error }, getTeamType] = useAxios({ url: `/api/teamType?page=1&pageSize=10`, method: 'GET' });
+    useEffect(() => {
+        if (teamTypeData) {
+            setParams({
+                ...params,
+                page: teamTypeData.page,
+                pageSize: teamTypeData.pageSize
+            });
+        }
+    }, [teamTypeData]);
+
+    const [{ data: typeData }, getType] = useAxios({
+        url: "../api/teamType?",
+      });
+
+    const handleSelectPage = (pageValue) => {
+        getteamType({ url: `/api/teamType?page=${pageValue}&pageSize=${params.pageSize}` })
+    };
+    const handleSelectPageSize = (sizeValue) => {
+        getteamType({ url: `/api/teamType?page=1&pageSize=${sizeValue}` })
+    };
+
+    if (loading) {
+        return <PageLoading />;
+    }
+    if (error) {
+        return <PageError />;
+    }
+    return (
+        <Container fluid className="pt-4 px-4">
+            <Card className="text-center rounded shadow p-4">
+                <div className="d-flex align-items-center justify-content-between mb-4">
+                    <Card.Title className="mb-0">
+                        รายการสินค้า
+                    </Card.Title>
+                    <teamTypeAddModal getData={getTeamType} getTypeData={typeData?.data}/>
+                </div>
+                <MyTable data={teamTypeData?.data} setNum={(teamTypeData?.page * teamTypeData?.pageSize) - teamTypeData?.pageSize} getData={getTeamType} getTypeData={typeData?.data} />
+                <MyPagination page={teamTypeData.page} totalPages={teamTypeData.totalPage} onChangePage={handleSelectPage} pageSize={params.pageSize} onChangePageSize={handleSelectPageSize} />
+            </Card >
+        </Container >
+    );
+}
+
+function MyTable(props) {
+    const [currentItems, setCurrentItems] = useState(props?.data);
+    const [numberSet, setNumberSet] = useState(props?.setNum);
+    useEffect(() => {
+        setCurrentItems(currentItems);
+        console.log(props);
+    }, [props]);
+
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>ประเภทย่อยสินค้า</th>
+                    <th>จัดการ</th>
+                </tr>
+            </thead>
+            <tbody>
+                {currentItems.length ? (
+                    currentItems?.map((item, index) => (
+                        <tr key={item.id}>
+                            <td>{index + 1 + numberSet}</td>
+                            <td>
+                                {item.name}
+                            </td>
+                            <td>
+                                <teamTypeEditModal value={item} getData={props?.getData} getTypeData={props?.getTypeData} />
+                                <teamTypeDeleteModal value={item} getData={props?.getData} />
+                            </td>
+                        </tr>
+                    )))
+                    : ""}
+            </tbody>
+        </Table>
+    );
+}
+
+
+teamTypePage.layout = IndexPage
