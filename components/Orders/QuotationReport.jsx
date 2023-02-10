1b0e759a011ad1f38f03343647c613c413634a4d
Ploy <117616170+yaminP@users.noreply.github.com>
2023-02-10 14:00:56 +0700

    pdf-print

--- components/Orders/QuotationReport.jsx
+++ components/Orders/QuotationReport.jsx
@@ -9,18 +9,14 @@ import {
   Table,
   FormControl,
 } from "react-bootstrap";
-import { FaTrash, FaPlus } from "react-icons/fa";
-import useAxios from "axios-hooks";
-import CardLoading from "@/components/CardChange/CardLoading";
-import CardError from "@/components/CardChange/CardError";
 import { TiDocumentText } from "react-icons/ti";
-import { format } from "date-fns";
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
-export default function ProductsDeleteModal(props) {
+import ReceiptData from "@/pages/ReceiptData";
+export default function ReceiptPrint(props) {
   const [showCheck, setShowCheck] = useState(false);
   const handleShow = () => setShowCheck(true);
   const handleClose = () => setShowCheck(false);
-  console.log(props.value);
+
+  // console.log(props.value);
   return (
     <>
       {props?.value?.status === "กำลังดำเนินการ" ? (
@@ -33,20 +29,11 @@ export default function ProductsDeleteModal(props) {
         </Button>
       ) : (
         ""
-      )}
-
-      <Modal show={showCheck} onHide={handleClose} centered size="xl">
-        <Modal.Header closeButton>
-          <Modal.Title>
-            ดูรายการสินค้า คุณ : {props?.value?.firstname}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-         
-        </Modal.Body>
-        <Modal.Footer></Modal.Footer>
-      </Modal>
+      )}  
+      {/* <Modal show={showCheck} onHide={handleClose} centered size="xl">
+          <ReceiptData/>
+      </Modal> */}
     </>
   );
-
 }
+
